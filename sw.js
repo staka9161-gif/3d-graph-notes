e276314e562d181600a46e8c7261ced23c8ded66
staka9161-gif <staka9161-gif@users.noreply.github.com>
2026-04-09 22:03:07 +0900

    3D Graph Notes - PWA for iPhone

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'graph-notes-v2';
+const ASSETS = ['./', './index.html', './icon.png', 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const clone = r.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+      return r;
+    }).catch(() => caches.match(e.request))
+  );
+});
